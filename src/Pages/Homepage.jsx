2560bc704994425ba6773dd9b34cb756181fc60d
Aayam Raj Shakya <aayamrajshakya@pm.me>
2024-10-15 08:00:25 -0500

    using img from assets

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import banner from '../Components/Assets/banner.png';
 import "./Homepage.css";
 
 export const Homepage = () => {
   return (
     <div className="background">
-        <img src={banner} className="banner"/>
+        <img src="/Assets/banner.png" className="banner"/>
         <h3>Nike's quality at Temu's price</h3>
     </div>
   )
